9861ebcfe50a46df044e548eccfc0d1e98828d4f
Nail <shaykhrazievnail@gmail.com>
2019-04-08 03:57:19 +0300

    Migrate to firestore

---
+++ app/src/main/java/com/itis/android/firebasesimple/activity/SignInActivity.kt
@@ -0,0 +1,205 @@
+package com.itis.android.firebasesimple.activity
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextUtils
+import android.text.TextWatcher
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.itis.android.firebasesimple.R
+import com.itis.android.firebasesimple.utils.SoftKeyboard
+import kotlinx.android.synthetic.main.activity_sign_in.*
+
+class SignInActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedListener {
+
+    private var googleApiClient: GoogleApiClient? = null
+    private var firebaseAuth: FirebaseAuth? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        initGoogleAuth()
+
+        // Initialize FirebaseAuth
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        if (firebaseAuth?.currentUser != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+        initClickListeners()
+        initTextListeners()
+    }
+
+    @SuppressLint("RestrictedApi")
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                task.getResult(ApiException::class.java)?.let {
+                    firebaseAuthWithGoogle(it)
+                } ?: run {
+                    Toast.makeText(this@SignInActivity, "Sign in error", Toast.LENGTH_SHORT).show()
+                }
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e)
+                // ...
+            }
+        }
+    }
+
+    override fun onConnectionFailed(connectionResult: ConnectionResult) {
+        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
+        // be available.
+        Log.d(TAG, "onConnectionFailed:$connectionResult")
+        Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun initClickListeners() {
+        btn_to_signup.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
+        btn_reset_password.setOnClickListener { }
+        btn_login.setOnClickListener {
+            val email = email.text.toString()
+            val password = password.text.toString()
+
+            if (TextUtils.isEmpty(email)) {
+                ti_email.error = getString(R.string.error_email)
+                return@setOnClickListener
+            }
+
+            if (TextUtils.isEmpty(password)) {
+                ti_password.error = getString(R.string.error_pass)
+                return@setOnClickListener
+            }
+
+            progressBar.visibility = View.VISIBLE
+            SoftKeyboard.hide(container)
+
+            //authenticate user
+            firebaseAuth?.signInWithEmailAndPassword(email, password)
+                    ?.addOnCompleteListener(this@SignInActivity) { task ->
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        progressBar.visibility = View.GONE
+                        if (!task.isSuccessful) {
+                            // there was an error
+                            if (password.length < 6) {
+                                ti_password.error = getString(R.string.error_pass_length)
+                            } else {
+                                Snackbar.make(container, R.string.error_signin, Snackbar.LENGTH_SHORT).show()
+                            }
+                        } else {
+                            val intent = Intent(this@SignInActivity, MainActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        }
+                    }
+        }
+    }
+
+    private fun initGoogleAuth() {
+        sign_in_button.setOnClickListener { signIn() }
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+        googleApiClient = GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build()
+    }
+
+    private fun handleFirebaseAuthResult(authResult: AuthResult?) {
+        if (authResult != null) {
+            // Welcome the user
+            val user = authResult.user
+            Toast.makeText(this, "Welcome ${user.email}", Toast.LENGTH_SHORT).show()
+
+            // Go back to the main activity
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.id!!)
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        firebaseAuth!!.signInWithCredential(credential)
+                .addOnCompleteListener(this) { task ->
+                    Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful)
+                    // If sign in fails, display a message to the user. If sign in succeeds
+                    // the auth state listener will be notified and logic to handle the
+                    // signed in user can be handled in the listener.
+                    if (!task.isSuccessful) {
+                        Log.w(TAG, "signInWithCredential", task.exception)
+                        Toast.makeText(this@SignInActivity, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show()
+                    } else {
+                        startActivity(Intent(this@SignInActivity, MainActivity::class.java))
+                        finish()
+                    }
+                }
+    }
+
+    private fun signIn() {
+        val signInIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient)
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    private fun initTextListeners() {
+        email.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable) {
+                ti_email.error = null
+            }
+        })
+        password.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable) {
+                ti_password.error = null
+            }
+        })
+    }
+
+    companion object {
+
+        private const val TAG = "SignInActivity"
+        private const val RC_SIGN_IN = 9001
+    }
+}
--- app/src/main/java/com/itis/android/firebasesimple/service/MyFirebaseMessagingService.java
+++ app/src/main/java/com/itis/android/firebasesimple/service/MyFirebaseMessagingService.java
@@ -1,18 +1,4 @@
-/**
- * Copyright Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */package com.itis.android.firebasesimple.service;
+package com.itis.android.firebasesimple.service;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
@@ -25,6 +11,7 @@ import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 import com.itis.android.firebasesimple.R;
@@ -33,6 +20,14 @@ import com.itis.android.firebasesimple.activity.MainActivity;
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     private static final String TAG = "MyFMService";
+    private static final String FRIENDLY_ENGAGE_TOPIC = "friendly_engage";
+
+    @Override
+    public void onNewToken(String token) {
+        super.onNewToken(token);
+        Log.d(TAG, "FCM Token: " + token);
+        FirebaseMessaging.getInstance().subscribeToTopic(FRIENDLY_ENGAGE_TOPIC);
+    }
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
@@ -52,7 +47,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         String channelId = "channelIdFCM";
 
-        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
 
         NotificationCompat.Builder notificationBuilder =
                 new NotificationCompat.Builder(this, channelId)
